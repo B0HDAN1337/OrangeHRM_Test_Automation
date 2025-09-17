3c9e6c9407839ed3e85597e2cde2e85127600501
B0HDAN1337 <machuliak1337@gmail.com>
2025-09-18 01:01:18 +0200

    fix

--- Python_PlayWright/pages/admin_page/UserManagementPage.py
+++ Python_PlayWright/pages/admin_page/UserManagementPage.py
@@ -6,12 +6,12 @@ class UserManagementPage:
     def __init__(self, page):
         self.page = page
 
-    def click_on_select(self, name_of_select):
+    def click_on_select_role(self, name_of_select):
        locator = AddUserLocator.select.format(name_of_select)
        self.page.wait_for_selector(locator)
        self.page.click(locator)
     
-    def dropdown_select(self, name):
+    def dropdown_select_role(self, name):
         locator = AddUserLocator.dropdown.format(name)
         self.page.wait_for_selector(locator)
         self.page.click(locator)
--- Python_PlayWright/tests/test_02_UserManagment.py
+++ Python_PlayWright/tests/test_02_UserManagment.py
@@ -4,7 +4,7 @@ from pages.admin_page.UserManagementPage import UserManagementPage
 def test_user_add(logged_in_user, base_url):
     logged_in_user.goto(f"{base_url}/web/index.php/admin/saveSystemUser")
     user_management = UserManagementPage(logged_in_user)
-    user_management.click_on_select("User Role")
-    user_management.dropdown_select("ESS")
+    user_management.click_on_select_role("User Role")
+    user_management.dropdown_select_role("ESS")
 
 
